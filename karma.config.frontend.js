6a865a48fd2af6a1f943ff5c8c0aed79872c6cc6
Antoine Lambert <antoine-e.lambert@thalesgroup.com>
2016-06-14 15:09:36 +0200

    capture console output when running frontend unit tests

--- karma.config.frontend.js
+++ karma.config.frontend.js
@@ -33,6 +33,9 @@ module.exports = function (config) {
   config.set({
     // tests will be executed in PhantomJS
     browsers: ['PhantomJS'],
+    client : {
+      captureConsole: true
+    },
     // code coverage output configuration : open ./coverage/html/index.html to see the report
     coverageReporter: {
       reporters: [
