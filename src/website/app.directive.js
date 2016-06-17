74ff171afbb6cc24fcf701bcad460e8c12d3451e
Antoine Lambert <antoine.lambert33@gmail.com>
2016-06-17 22:22:00 +0200

    make the current navbar link active after clicking on it

--- src/website/app.directive.js
+++ src/website/app.directive.js
@@ -1,9 +1,20 @@
 import template from './app.html';
 
 class AppDirective {
-  constructor() {
+  constructor($timeout) {
+    'ngInject';
+    this.$timeout = $timeout;
     this.template = template;
     this.restrict = 'E';
+  }
+
+  link(scope, element) {
+    this.$timeout(() => {
+      $(".nav li a").on("click", function(){
+        $(".nav").find(".active").removeClass("active");
+        $(this).parent().addClass("active");
+      });
+    }, false);
   }
 }
 
--- src/website/app.js
+++ src/website/app.js
@@ -15,18 +15,30 @@ import uiRouter from 'angular-ui-router';
 const app = registerAngularModule('app', [services.name, directives.module.name, panels.module.name, uiRouter])
               .controller('appCtrl', function($scope) {
                 'ngInject';
-                $scope.datasets = ['Cars', 'Cameras'];
+                $scope.datasets = [
+                  {
+                    name: 'Cars',
+                    url: 'cars',
+                    icon: 'car'
+                  },
+                  {
+                    name: 'Cameras',
+                    url: 'cameras',
+                    icon: 'camera'
+                  },
+
+                ];
               })
               .directive('app', AppDirective)
               .config(($stateProvider, $urlRouterProvider) => {
                 'ngInject';
-                $urlRouterProvider.otherwise('/Cars');
-                $stateProvider.state('Cars', {
-                                      url: '/Cars',
+                $urlRouterProvider.otherwise('/cars');
+                $stateProvider.state('cars', {
+                                      url: '/cars',
                                       template: '<workspace data-dataset-id="cars"/>',
                                      })
-                              .state('Cameras', {
-                                      url: '/Cameras',
+                              .state('cameras', {
+                                      url: '/cameras',
                                       template: '<workspace data-dataset-id="cameras"/>',
                                     });
               });
