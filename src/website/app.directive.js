e6d59b36056f3b4c2de82307744e6acec40ac294
Antoine Lambert <antoine-e.lambert@thalesgroup.com>
2016-07-07 10:43:51 +0200

    simplify the way to handle active link in the navbar

--- src/website/app.directive.js
+++ src/website/app.directive.js
@@ -9,17 +9,6 @@ class AppDirective {
     this.restrict = 'E';
   }
 
-  link(scope, element) {
-    // $timeout does not get injected when running unit tests (don't really know why ...)
-    if (!this.$timeout) return;
-    this.$timeout(() => {
-      $($(".nav li a")[0]).parent().addClass("active");
-      $(".nav li a").on("click", function(){
-        $(".nav").find(".active").removeClass("active");
-        $(this).parent().addClass("active");
-      });
-    }, false);
-  }
 }
 
 export default AppDirective;
